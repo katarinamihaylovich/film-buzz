5977e3a4c57028654f30cc9d04ca00f549b98c24
Daniel Xu <xudh47@gmail.com>
2022-08-17 19:18:39 -0700

    Added psuedo code for work distribution.' did not match any file(s) known to git.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,41 @@
-var moviesAPI = `https://www.omdbapi.com/?apikey=e1fca0fc&t=${search}`;
-
-var drinksAPI =
-  "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail";
-
 var searchBtn = document.querySelector("#search-btn");
 var searchInput = document.querySelector("#search-input");
-var search = searchInput.value;
-console.log(search);
-// var search = "titanic";
-// Genre, Time, Ratings
-// hardcode
 
 function submit() {
-  fetch(moviesAPI)
+  var search = searchInput.value.toString();
+  console.log(search);
+
+  // Fetch request for moviesAPI
+  fetch(`https://www.omdbapi.com/?apikey=e1fca0fc&t=${search}`)
     .then(function (response) {
       console.log(response);
       return response.json();
     })
     .then(function (data) {
       console.log(data);
-      // if data.genre = horror
-      // drink cat = gin
-      // fetch(drinks gin)
+      console.log(data.Genre);
+      // Each genre
+      console.log(data.Genre.split(","));
+      // One genre
+      console.log(data.Genre.split(",")[0]);
 
-      fetch(drinksAPI)
+      // DOM for movie display
+      // Set up variables for APIs needed for drinks first so the person working on drinks have access to data from movie fetch
+
+      fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail")
         .then(function (response) {
           console.log(response);
           return response.json();
         })
         .then(function (data) {
           console.log(data);
+          // if genre = x then drink
+          // create an array that includes a list of drinks for each genre. Some movies have multiple genres
+          // create a random number generator that selects a drink from the array for display
+
+          // if ratings = x then drink // below a certain score
+          // create an array that includes a list of drinks for each genre
+          // create a random number generator that selects a drink from the array for display
         });
     });
 }
@@ -37,14 +43,17 @@ function submit() {
 // Search Button
 searchBtn.addEventListener("click", submit);
 
+// Ideas
+// 1. A second button called feeling lucky which includes an array of movies we put in there. We then make a random number generator
+// to pick the movies from the bracket.
+
+// 2. A
+
+// NOTES / NOT PART OF CODE
 // call function inside fetch // not to be used
 // function callMyFunc(someData) {
 //   console.log(someData);
 // }
-
-// if/else pertaining to genre
-// if/else pertaining to time
-// if/else pertaining to ratings
 
 // OMDB
 // horof35563@wnpop.com
@@ -53,10 +62,6 @@ searchBtn.addEventListener("click", submit);
 // IMDB
 // https://imdb-api.com/en/API/SearchKeyword/k_12345678/gun
 
-// 1. Search by title
-// 2. Genre
-// 3. Runtime
-// 4. Ratings
-
-// if genre = action then
-// if ratings = then
+// if data.genre = horror
+// drink cat = gin
+// fetch(drinks gin)
