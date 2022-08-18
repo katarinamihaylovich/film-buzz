5b24b7a700304e696963772f0a07a4a505ca2c77
Randa Bukowski <randabukowski@Randas-MacBook-Pro.local>
2022-08-17 21:56:31 -0700

    drinks

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,51 @@ function submit() {
         })
         .then(function (data) {
           console.log(data);
+          if ( ) {
+
+          }
+
+
+
+          Action = ['Manhattan', 'Aviation', 'Gimlet' ]
+          Adventure = ['Blue Hurricane', 'Midnight Mint', 'Greyhound' ]
+          Romance = ['Cosmopolitan', 'Lemon Drop', 'Adam & Eve' ]
+          Fantasy = ['Dry Martini', 'Gin and Tonic', 'Imperial Cocktail'  ]
+          Comedy = [ 'Banana Cream Pi', 'Flaming Lamborghini', 'Gin Basil Smash '  ]
+          Drama = [ '57 Checy with a White License Plate', '747 Drink', '155 Belmont' ]
+          Horror = ['Michelada', 'Apperol Spritz', 'Amaretto Fizz' ]
+          Thriller = ['Gin Basil Smash', 'Broadside', ''  ]
+          Sci-Fi = ['Espresso Martini', 'Hot Toddy', ''  ]
+          Crime = ['Miama Vice', 'Dark Caipirinha', "Hunter's Moon" ]
+          Animation = ['Mango Mojito', 'Lemon Elderflower Spritzer', 'Cream Soda'  ]
+          Mystery = [ 'Adios Amigos Cocktail', 'Corpse Reviver', 'Death in the Afternoon' ]
+          Biography = [ 'Gin Rickey', 'Addison', 'Mary Pickford' ]
+          Documentary = ['Irish Curlding Cow', 'Mountain Bramble', "Duchamp's Punch"  ]
+          Family = ['Absolutely Fabulous', 'Cherry Electric Lemonade', 'Applejack'  ]
+          Film-Noir = [ 'Butterfly Effect', 'Afterglow', 'French Martini']
+          History = ['Brooklyn', 'Malibu Twister', " A Gillian's Island"   ]
+          Music = [ 'Funk and Soul', 'A1', 'Ace'  ]
+          Musical = [ 'Jitterbug', 'Honey Bee', 'Lazy Coconut Paloma'  ]
+          Short = [ 'Bahama Mama', 'Autumn Garibaldi', 'Figgy Thyme'  ]
+          Sport = ['A True Amaretto Sour',"Arizona Stingers' ", '9 1/2 Weeks' ]
+          Western = ['Bounty Hunter', 'Corn n Oil', "Cocktail Horse's Neck"  ]
+          War = [ 'Absolutly Screwed Up', 'Army Special', 'Bombay Cassis' ]
+          Superhero = ["Captain kidd's Punch", 'Aquamarine', 'Apple Karate'  ]
+
+
+
+
+
+
+
+
+
+
+
+          
+      
+    
+          
           // if genre = x then drink
           // create an array that includes a list of drinks for each genre. Some movies have multiple genres
           // create a random number generator that selects a drink from the array for display
